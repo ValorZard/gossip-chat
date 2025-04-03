d981a3044525e1610ac3bad12a1666998ac13d1d
Srayan Jana <valorzard.kiko@outlook.com>
2025-04-02 20:23:29 -0700

    BREAK EVERYTHING

--- src/main.rs
+++ src/main.rs
@@ -2,12 +2,18 @@ use std::{collections::HashMap, fmt, str::FromStr};
 
 use anyhow::Result;
 use clap::Parser;
+use crossterm::event::{self, Event as CrosstermEvent, KeyCode, KeyEventKind};
 use futures_lite::StreamExt;
-use iroh::{protocol::Router, Endpoint, NodeAddr, NodeId};
+use iroh::{Endpoint, NodeAddr, NodeId, protocol::Router};
 use iroh_gossip::{
     net::{Event, Gossip, GossipEvent, GossipReceiver},
     proto::TopicId,
 };
+use ratatui::layout::{Constraint, Layout, Position};
+use ratatui::style::{Color, Modifier, Style, Stylize};
+use ratatui::text::{Line, Span, Text};
+use ratatui::widgets::{Block, List, ListItem, Paragraph};
+use ratatui::{DefaultTerminal, Frame};
 use serde::{Deserialize, Serialize};
 
 /// Chat over iroh-gossip
@@ -45,90 +51,11 @@ async fn main() -> Result<()> {
     let args = Args::parse();
 
     // parse the cli command
-    let (topic, nodes) = match &args.command {
-        Command::Open => {
-            let topic = TopicId::from_bytes(rand::random());
-            println!("> opening chat room for topic {topic}");
-            (topic, vec![])
-        }
-        Command::Join { ticket } => {
-            let Ticket { topic, nodes } = Ticket::from_str(ticket)?;
-            println!("> joining chat room for topic {topic}");
-            (topic, nodes)
-        }
-    };
-
-    let endpoint = Endpoint::builder().discovery_n0().bind().await?;
-
-    println!("> our node id: {}", endpoint.node_id());
-    let gossip = Gossip::builder().spawn(endpoint.clone()).await?;
-
-    let router = Router::builder(endpoint.clone())
-        .accept(iroh_gossip::ALPN, gossip.clone())
-        .spawn()
-        .await?;
-
-    // in our main file, after we create a topic `id`:
-    // print a ticket that includes our own node id and endpoint addresses
-    let ticket = {
-        // Get our address information, includes our
-        // `NodeId`, our `RelayUrl`, and any direct
-        // addresses.
-        let me = endpoint.node_addr().await?;
-        let nodes = vec![me];
-        Ticket { topic, nodes }
-    };
-    println!("> ticket to join us: {ticket}");
-
-    // join the gossip topic by connecting to known nodes, if any
-    let node_ids = nodes.iter().map(|p| p.node_id).collect();
-    if nodes.is_empty() {
-        println!("> waiting for nodes to join us...");
-    } else {
-        println!("> trying to connect to {} nodes...", nodes.len());
-        // add the peer addrs from the ticket to our endpoint's addressbook so that they can be dialed
-        for node in nodes.into_iter() {
-            endpoint.add_node_addr(node)?;
-        }
-    };
-    let (sender, receiver) = gossip.subscribe_and_join(topic, node_ids).await?.split();
-    println!("> connected!");
-
-    // broadcast our name, if set
-    if let Some(name) = args.name {
-        let message = Message::AboutMe {
-            from: endpoint.node_id(),
-            name,
-        };
-        sender.broadcast(message.to_vec().into()).await?;
-    }
-
-    // subscribe and print loop
-    tokio::spawn(subscribe_loop(receiver));
-
-    // spawn an input thread that reads stdin
-    // create a multi-provider, single-consumer channel
-    let (line_tx, mut line_rx) = tokio::sync::mpsc::channel(1);
-    // and pass the `sender` portion to the `input_loop`
-    std::thread::spawn(move || input_loop(line_tx));
-
-    // broadcast each line we type
-    println!("> type a message and hit enter to broadcast...");
-    // listen for lines that we have typed to be sent from `stdin`
-    while let Some(text) = line_rx.recv().await {
-        // create a message from the text
-        let message = Message::Message {
-            from: endpoint.node_id(),
-            text: text.clone(),
-        };
-        // broadcast the encoded message
-        sender.broadcast(message.to_vec().into()).await?;
-        // print to ourselves the text that we sent
-        println!("> sent: {text}");
-    }
-    router.shutdown().await?;
-
-    Ok(())
+    let app = App::new(&args)?;
+    let terminal = ratatui::init();
+    let result = app.run(terminal).await;
+    ratatui::restore();
+    result
 }
 
 #[derive(Debug, Serialize, Deserialize)]
@@ -226,5 +153,298 @@ impl FromStr for Ticket {
     fn from_str(s: &str) -> Result<Self, Self::Err> {
         let bytes = data_encoding::BASE32_NOPAD.decode(s.to_ascii_uppercase().as_bytes())?;
         Self::from_bytes(&bytes)
+    }
+}
+
+/// App holds the state of the application
+struct App {
+    /// Current value of the input box
+    input: String,
+    /// Position of cursor in the editor area.
+    character_index: usize,
+    /// Current input mode
+    input_mode: InputMode,
+    /// History of recorded messages
+    messages: Vec<String>,
+    /// Topic Id
+    topic: TopicId,
+    /// Node addresses
+    nodes: Vec<NodeAddr>,
+    endpoint: Option<Endpoint>,
+    router: Option<Router>,
+    name: Option<String>,
+}
+
+enum InputMode {
+    Normal,
+    Editing,
+}
+
+impl App {
+    fn new(args: &Args) -> Result<Self> {
+        let (topic, nodes) = match &args.command {
+            Command::Open => {
+                let topic = TopicId::from_bytes(rand::random());
+                println!("> opening chat room for topic {topic}");
+                (topic, vec![])
+            }
+            Command::Join { ticket } => {
+                let Ticket { topic, nodes } = Ticket::from_str(&ticket)?;
+                println!("> joining chat room for topic {topic}");
+                (topic, nodes)
+            }
+        };
+
+        Ok(Self {
+            input: String::new(),
+            input_mode: InputMode::Normal,
+            messages: Vec::new(),
+            character_index: 0,
+            topic,
+            nodes,
+            name: args.name.clone(),
+            endpoint: None,
+            router: None,
+        })
+    }
+
+    fn move_cursor_left(&mut self) {
+        let cursor_moved_left = self.character_index.saturating_sub(1);
+        self.character_index = self.clamp_cursor(cursor_moved_left);
+    }
+
+    fn move_cursor_right(&mut self) {
+        let cursor_moved_right = self.character_index.saturating_add(1);
+        self.character_index = self.clamp_cursor(cursor_moved_right);
+    }
+
+    fn enter_char(&mut self, new_char: char) {
+        let index = self.byte_index();
+        self.input.insert(index, new_char);
+        self.move_cursor_right();
+    }
+
+    /// Returns the byte index based on the character position.
+    ///
+    /// Since each character in a string can contain multiple bytes, it's necessary to calculate
+    /// the byte index based on the index of the character.
+    fn byte_index(&self) -> usize {
+        self.input
+            .char_indices()
+            .map(|(i, _)| i)
+            .nth(self.character_index)
+            .unwrap_or(self.input.len())
+    }
+
+    fn delete_char(&mut self) {
+        let is_not_cursor_leftmost = self.character_index != 0;
+        if is_not_cursor_leftmost {
+            // Method "remove" is not used on the saved text for deleting the selected char.
+            // Reason: Using remove on String works on bytes instead of the chars.
+            // Using remove would require special care because of char boundaries.
+
+            let current_index = self.character_index;
+            let from_left_to_current_index = current_index - 1;
+
+            // Getting all characters before the selected character.
+            let before_char_to_delete = self.input.chars().take(from_left_to_current_index);
+            // Getting all characters after selected character.
+            let after_char_to_delete = self.input.chars().skip(current_index);
+
+            // Put all characters together except the selected one.
+            // By leaving the selected one out, it is forgotten and therefore deleted.
+            self.input = before_char_to_delete.chain(after_char_to_delete).collect();
+            self.move_cursor_left();
+        }
+    }
+
+    fn clamp_cursor(&self, new_cursor_pos: usize) -> usize {
+        new_cursor_pos.clamp(0, self.input.chars().count())
+    }
+
+    fn reset_cursor(&mut self) {
+        self.character_index = 0;
+    }
+
+    fn submit_message(&mut self) {
+        self.messages.push(self.input.clone());
+        self.input.clear();
+        self.reset_cursor();
+    }
+
+    async fn run(mut self, mut terminal: DefaultTerminal) -> Result<()> {
+        let endpoint = Endpoint::builder().discovery_n0().bind().await?;
+
+        println!("> our node id: {}", endpoint.node_id());
+        let gossip = Gossip::builder().spawn(endpoint.clone()).await?;
+
+        let router = Router::builder(endpoint.clone())
+            .accept(iroh_gossip::ALPN, gossip.clone())
+            .spawn()
+            .await?;
+
+        // in our main file, after we create a topic `id`:
+        // print a ticket that includes our own node id and endpoint addresses
+        let ticket = {
+            // Get our address information, includes our
+            // `NodeId`, our `RelayUrl`, and any direct
+            // addresses.
+            let me = endpoint.node_addr().await?;
+            let nodes = vec![me];
+            Ticket {
+                topic: self.topic,
+                nodes,
+            }
+        };
+        println!("> ticket to join us: {ticket}");
+
+        // join the gossip topic by connecting to known nodes, if any
+        let node_ids = self.nodes.iter().map(|p| p.node_id).collect();
+        if self.nodes.is_empty() {
+            println!("> waiting for nodes to join us...");
+        } else {
+            println!("> trying to connect to {} nodes...", self.nodes.len());
+            // add the peer addrs from the ticket to our endpoint's addressbook so that they can be dialed
+            for node in self.nodes.clone().into_iter() {
+                endpoint.add_node_addr(node)?;
+            }
+        };
+        let (sender, receiver) = gossip
+            .subscribe_and_join(self.topic, node_ids)
+            .await?
+            .split();
+        println!("> connected!");
+
+        // broadcast our name, if set
+        if let Some(ref name) = self.name {
+            let message = Message::AboutMe {
+                from: endpoint.node_id(),
+                name: name.to_string(),
+            };
+            sender.broadcast(message.to_vec().into()).await?;
+        }
+
+        // subscribe and print loop
+        tokio::spawn(subscribe_loop(receiver));
+
+        // spawn an input thread that reads stdin
+        // create a multi-provider, single-consumer channel
+        let (line_tx, mut line_rx) = tokio::sync::mpsc::channel(1);
+        // and pass the `sender` portion to the `input_loop`
+        std::thread::spawn(move || input_loop(line_tx));
+
+        // broadcast each line we type
+        println!("> type a message and hit enter to broadcast...");
+        // listen for lines that we have typed to be sent from `stdin`
+        while let Some(text) = line_rx.recv().await {
+            // create a message from the text
+            let message = Message::Message {
+                from: endpoint.node_id(),
+                text: text.clone(),
+            };
+            // broadcast the encoded message
+            sender.broadcast(message.to_vec().into()).await?;
+            // print to ourselves the text that we sent
+            println!("> sent: {text}");
+        }
+
+        loop {
+            terminal.draw(|frame| self.draw(frame))?;
+
+            if let CrosstermEvent::Key(key) = event::read()? {
+                match self.input_mode {
+                    InputMode::Normal => match key.code {
+                        KeyCode::Char('e') => {
+                            self.input_mode = InputMode::Editing;
+                        }
+                        KeyCode::Char('q') => {
+                            router.shutdown().await?;
+                            return Ok(());
+                        }
+                        _ => {}
+                    },
+                    InputMode::Editing if key.kind == KeyEventKind::Press => match key.code {
+                        KeyCode::Enter => self.submit_message(),
+                        KeyCode::Char(to_insert) => self.enter_char(to_insert),
+                        KeyCode::Backspace => self.delete_char(),
+                        KeyCode::Left => self.move_cursor_left(),
+                        KeyCode::Right => self.move_cursor_right(),
+                        KeyCode::Esc => self.input_mode = InputMode::Normal,
+                        _ => {}
+                    },
+                    InputMode::Editing => {}
+                }
+            }
+        }
+    }
+
+    fn draw(&self, frame: &mut Frame) {
+        let vertical = Layout::vertical([
+            Constraint::Length(1),
+            Constraint::Length(3),
+            Constraint::Min(1),
+        ]);
+        let [help_area, input_area, messages_area] = vertical.areas(frame.area());
+
+        let (msg, style) = match self.input_mode {
+            InputMode::Normal => (
+                vec![
+                    "Press ".into(),
+                    "q".bold(),
+                    " to exit, ".into(),
+                    "e".bold(),
+                    " to start editing.".bold(),
+                ],
+                Style::default().add_modifier(Modifier::RAPID_BLINK),
+            ),
+            InputMode::Editing => (
+                vec![
+                    "Press ".into(),
+                    "Esc".bold(),
+                    " to stop editing, ".into(),
+                    "Enter".bold(),
+                    " to record the message".into(),
+                ],
+                Style::default(),
+            ),
+        };
+        let text = Text::from(Line::from(msg)).patch_style(style);
+        let help_message = Paragraph::new(text);
+        frame.render_widget(help_message, help_area);
+
+        let input = Paragraph::new(self.input.as_str())
+            .style(match self.input_mode {
+                InputMode::Normal => Style::default(),
+                InputMode::Editing => Style::default().fg(Color::Yellow),
+            })
+            .block(Block::bordered().title("Input"));
+        frame.render_widget(input, input_area);
+        match self.input_mode {
+            // Hide the cursor. `Frame` does this by default, so we don't need to do anything here
+            InputMode::Normal => {}
+
+            // Make the cursor visible and ask ratatui to put it at the specified coordinates after
+            // rendering
+            #[allow(clippy::cast_possible_truncation)]
+            InputMode::Editing => frame.set_cursor_position(Position::new(
+                // Draw the cursor at the current position in the input field.
+                // This position can be controlled via the left and right arrow key
+                input_area.x + self.character_index as u16 + 1,
+                // Move one line down, from the border to the input line
+                input_area.y + 1,
+            )),
+        }
+
+        let messages: Vec<ListItem> = self
+            .messages
+            .iter()
+            .enumerate()
+            .map(|(i, m)| {
+                let content = Line::from(Span::raw(format!("{i}: {m}")));
+                ListItem::new(content)
+            })
+            .collect();
+        let messages = List::new(messages).block(Block::bordered().title("Messages"));
+        frame.render_widget(messages, messages_area);
     }
 }
